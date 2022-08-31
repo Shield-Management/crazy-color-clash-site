212399dd1661d71fb7f80f604805f4eb11b3c427
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-08-30 23:55:41 -0600

    feat: finishes website with images, styling, and content

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -26,14 +26,16 @@
 
 <body>
     <div class="bg-banner text-center">
-        <img src="{{ asset('pics/icon.png') }}" class="game-image">
+        <img src="{{ asset('pics/icon.png') }}" class="app-icon">
         <h1>Crazy Color Clash</h1>
-        <h3>Crazy colorful tapping battles</h3>
+        <h2>Crazy colorful tapping battles</h2>
+
+        <br />
 
         <a href="https://testflight.apple.com/join/5Vaqo19P">
             <object data="{{ asset('pics/app-store-logo.svg') }}" width="200" class="app-store-icon p-3"
                 style="pointer-events: none;"></object>
-            <p>Join the public beta today!</p>
+            <h3>Join the public beta today!</h3>
         </a>
 
         <div class="row">
@@ -48,11 +50,26 @@
             </div>
         </div>
 
-        <p class="p-3">By: Justin Hammond</p>
+        <p class="p-3">Developed By: <a href="https://justinpaulhammond.com">Justin Hammond</a></p>
     </div>
 
     <div class="container">
         <h2>Concept</h2>
+        <div class="row text-center">
+            <div class="col-sm-3">
+                <p class="capsule">2 PLAYERS</p>
+            </div>
+            <div class="col-sm-3">
+                <p class="capsule">1 DEVICE</p>
+            </div>
+            <div class="col-sm-3">
+                <p class="capsule">90 SECONDS</p>
+            </div>
+            <div class="col-sm-3">
+                <p class="capsule">12 POWERUPS</p>
+            </div>
+        </div>
+        <br />
         <p class="text-center">Tap your half of the screen when your background color matches the color name shown.
             Watch out, the names of colors may not always match the color of the name or your background color!
             Points
@@ -63,79 +80,133 @@
             when time runs out. Use your powerups to confuse and disorient your opponent!</p>
     </div>
 
-    <div class="container text-center">
-        <h2>Beta Testing</h2>
-        <p>Crazy Color Clash is currently in beta testing and is close to release. We could use your help by having you
-            download the app for free and giving it a try. We appreciate any and all feedback to help us polish the game
-            before its final release.</p>
-        <a href="https://testflight.apple.com/join/5Vaqo19P">
-            <object data="{{ asset('pics/app-store-logo.svg') }}" width="200" class="app-store-icon p-3"
-                style="pointer-events: none;"></object>
-            <p>Join the public beta today!</p>
-        </a>
+    <div class="bg-banner">
+        <div class="container text-center">
+            <h2>Beta Testing</h2>
+            <p>Crazy Color Clash is currently in beta testing in preperation for its <strong>September/October 2022
+                    release</strong>. We
+                could use your help by having
+                you
+                download the app for free and giving it a try. We appreciate any and all feedback to help us polish the
+                game
+                before it hits the app store.</p>
+            <a href="https://testflight.apple.com/join/5Vaqo19P">
+                <object data="{{ asset('pics/app-store-logo.svg') }}" width="200" class="app-store-icon p-3"
+                    style="pointer-events: none;"></object>
+                <p>Join the public beta today!</p>
+            </a>
+        </div>
     </div>
 
     <div class="container text-center">
         <h2>12 Exciting Powerups</h2>
+        <p>There are various powerups that are granted for correct taps. Use your powerups to confuse and disorient your
+            opponent!</p>
+        <p>During the public beta, all in-app purchases are <strong>free</strong>! Three powerups are included in the
+            final release for
+            free while additional powerup packs can be purchased for $0.99.</p>
+    </div>
 
-        <div class="row">
-            <h3>Basic Powerups</h3>
-            <div class="col">
-                <h4>Surprise</h4>
-                <img src="{{ asset('pics/surprise-min.jpg') }}" class="game-image img-fluid">
-            </div>
-            <div class="col">
-                <h4>Lockout</h4>
-                <img src="{{ asset('pics/lockout-min.jpg') }}" class="game-image img-fluid">
-            </div>
-            <div class="col">
-                <h4>Blackout</h4>
-                <img src="{{ asset('pics/blackout-min.jpg') }}" class="game-image img-fluid">
+    <div class="bg-pink">
+        <div class="container text-center">
+            <div class="row">
+                <h3>Basic Powerups</h3>
+                <p>Free - Included in Game</p>
+                <div class="col">
+                    <h4>Surprise</h4>
+                    <img src="{{ asset('pics/surprise-min.png') }}" class="game-image img-fluid">
+                    <p>Shoots a colorful confetti cannon on your opponent's screen to distract them.</p>
+                </div>
+                <div class="col">
+                    <h4>Lockout</h4>
+                    <img src="{{ asset('pics/lockout-min.png') }}" class="game-image img-fluid">
+                    <p>Locks your opponents screen. They must tap 5 times before they can match colors again. Powerups
+                        cannot be used during Lockout!</p>
+                </div>
+                <div class="col">
+                    <h4>Blackout</h4>
+                    <img src="{{ asset('pics/blackout-min.png') }}" class="game-image img-fluid">
+                    <p>Changes your opponent's background color to black making it impossible to match colors. Taps
+                        during a
+                        Blackout lose points!</p>
+                </div>
             </div>
         </div>
+    </div>
 
-        <hr />
-
-        <div class="row">
-            <h3>Powerup Pack 1</h3>
-            <div class="col">
-                <h4>Boring</h4>
-            </div>
-            <div class="col">
-                <h4>Bombs</h4>
-            </div>
-            <div class="col">
-                <h4>Clash</h4>
+    <div class="bg-purple">
+        <div class="container text-center">
+            <div class="row">
+                <h3>Powerup Pack 1</h3>
+                <p>$0.99 - Unlocks 3 powerups</p>
+                <div class="col">
+                    <h4>Boring</h4>
+                    <img src="{{ asset('pics/boring-min.png') }}" class="game-image img-fluid">
+                    <p>Removes the changing color name's font color, making it easier to match taps.</p>
+                </div>
+                <div class="col">
+                    <h4>Bombs</h4>
+                    <img src="{{ asset('pics/bombs-min.png') }}" class="game-image img-fluid">
+                    <p>Replaces your opponent's powerups with bombs. Tapping a bomb will lose a point!</p>
+                </div>
+                <div class="col">
+                    <h4>Clash</h4>
+                    <img src="{{ asset('pics/clash-min.png') }}" class="game-image img-fluid">
+                    <p>Starts a duel with your opponent. Tapping shifts the count in your favor. First to 10 taps
+                        receives 5
+                        points.</p>
+                </div>
             </div>
         </div>
+    </div>
 
-        <hr />
-
-        <div class="row">
-            <h3>Powerup Pack 2</h3>
-            <div class="col">
-                <h4>Reverse</h4>
-            </div>
-            <div class="col">
-                <h4>Monochrome</h4>
-            </div>
-            <div class="col">
-                <h4>Nuke</h4>
+    <div class="bg-blue">
+        <div class="container text-center">
+            <div class="row">
+                <h3>Powerup Pack 2</h3>
+                <p>$0.99 - Unlocks 3 powerups</p>
+                <div class="col">
+                    <h4>Reverse</h4>
+                    <img src="{{ asset('pics/reverse-min.png') }}" class="game-image img-fluid">
+                    <p>Reverses your opponent's color name direction.</p>
+                </div>
+                <div class="col">
+                    <h4>Monochrome</h4>
+                    <img src="{{ asset('pics/monochrome-min.png') }}" class="game-image img-fluid">
+                    <p>Changes your opponent's background to gray. They will need to guess if their colors match!</p>
+                </div>
+                <div class="col">
+                    <h4>Nuke</h4>
+                    <img src="{{ asset('pics/nuke-min.png') }}" class="game-image img-fluid">
+                    <p>Blows away all of your opponents powerups.</p>
+                </div>
             </div>
         </div>
+    </div>
 
-        <hr />
-
-        <div class="row">
-            <h3>Powerup Pack 3</h3>
-            <div class="col">
-                <h4>Thief</h4>
-            </div>
-            <div class="col">
-                <h4>Double Tap</h4>
-            </div>
-            <div class="col">
-                <h4>Shield</h4>
+    <div class="bg-green">
+        <div class="container text-center">
+            <div class="row">
+                <h3>Powerup Pack 3</h3>
+                <p>$0.99 - Unlocks 3 powerups</p>
+                <div class="col">
+                    <h4>Thief</h4>
+                    <img src="{{ asset('pics/thief-min.png') }}" class="game-image img-fluid">
+                    <p>75% chance of stealing a point from your opponent. Be careful, the other 25% of the time you will
+                        lose a point yourself!</p>
+                </div>
+                <div class="col">
+                    <h4>Double Tap</h4>
+                    <img src="{{ asset('pics/double-tap-min.png') }}" class="game-image img-fluid">
+                    <p>Requires your opponent to double tap their color on a match. Failing to double tap will lose a
+                        point!
+                    </p>
+                </div>
+                <div class="col">
+                    <h4>Shield</h4>
+                    <img src="{{ asset('pics/shield-min.png') }}" class="game-image img-fluid">
+                    <p>Protects you against powerups.</p>
+                </div>
             </div>
         </div>
     </div>
@@ -146,7 +217,7 @@
                 Policy</a>
             <a href="/terms-and-conditions" class="nav-link">Terms and
                 Conditions</a>
-            <p>&#169; Justin Hammond, All rights reserved.</p>
+            <p>&#169; <a href="https://justinpaulhammond.com">Justin Hammond</a>, All rights reserved.</p>
         </div>
     </footer>
 

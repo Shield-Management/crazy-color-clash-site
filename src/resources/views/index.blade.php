48eb6ad99046063f2910ea45788e62846429a264
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-09-21 20:56:47 -0600

    makes changes for final release

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -10,6 +10,7 @@
     <script src="https://kit.fontawesome.com/0dd4ecd465.js" crossorigin="anonymous"></script>
     <meta name="keywords" content="crazy,color,clash,ios,apple,game,app,phone,player">
     <meta name="description" content="Crazy Color Clash by Justin Hammond - Crazy colorful tapping battles.">
+    <meta name="apple-itunes-app" content="app-id=1641436535">{{-- iOS download banner --}}
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-W34NSQXHFR"></script>
     <script>
@@ -33,11 +34,10 @@
 
             <br />
 
-            <a href="https://testflight.apple.com/join/5Vaqo19P">
-                <object data="{{ asset('pics/app-store-logo.svg') }}" width="200" class="app-store-icon p-3"
-                    style="pointer-events: none;"></object>
-                <h3>Join the public beta today!</h3>
-            </a>
+            <a href="https://apps.apple.com/us/app/crazy-color-clash/id1641436535?itsct=apps_box_badge&amp;itscg=30200"
+                style="display: inline-block; overflow: hidden; border-radius: 13px; width: 250px; height: 83px;"><img
+                    src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1663718400&h=35a801062337f87a46a0f0c6b1f38ae1"
+                    alt="Download on the App Store" style="border-radius: 13px; width: 250px; height: 83px;"></a>
 
             <div class="row">
                 <div class="col">
@@ -47,7 +47,7 @@
                     <img src="{{ asset('pics/tutorial-min.png') }}" class="game-image img-fluid">
                 </div>
                 <div class="col">
-                    <img src="{{ asset('pics/menu-min.png') }}" class="game-image img-fluid">
+                    <img src="{{ asset('pics/score-min.png') }}" class="game-image img-fluid">
                 </div>
             </div>
 
@@ -57,6 +57,12 @@
 
     <div class="container">
         <h2>Concept</h2>
+        <p class="text-center"><a
+                href="https://apps.apple.com/us/app/crazy-color-clash/id1641436535?itsct=apps_box_link&itscg=30200">Crazy
+                Color Clash</a> - the game with 2 players, 1 device, 12 powerups, and 90 seconds. Play
+            in one of 3 incredibly colorful game modes, clash with a friend or fly solo in practice mode. Use powerups
+            to confuse and disorient your opponent and come out the victor once the time runs out.</p>
+
         <div class="row text-center">
             <div class="col-lg-3">
                 <p class="capsule">2 PLAYERS</p>
@@ -71,48 +77,41 @@
                 <p class="capsule">12 POWERUPS</p>
             </div>
         </div>
+
         <br />
+
         <p class="text-center">Tap your half of the screen when your background color matches the color name shown.
-            Watch out, the names of colors may not always match the color of the name or your background color!
-            Points
-            are gained for matching color names to background colors, points lost for incorrect taps, and points
-            forfeit
-            for missed taps. There are various powerups that are granted for correct taps. Winner has the most
-            points
-            when time runs out. Use your powerups to confuse and disorient your opponent!</p>
-    </div>
+            Watch out, the names of
+            colors may not always match the color of the name or your background color! Points are gained for matching
+            color names to background colors, points lost for incorrect taps, and points forfeit for missed taps. There
+            are 12 powerups that are granted throughout the game for correct taps. Mix and match to find the best
+            combination against your opponent or skip powerups completely and stick to colors alone. Winner has the most
+            points when time runs out. Do you have what it takes to win, or will you tap out?</p>
 
-    <div class="bg-banner">
-        <div class="container text-center">
-            <h2>Beta Testing</h2>
-            <p>Crazy Color Clash is currently in beta testing in preperation for its <strong>September/October 2022
-                    release</strong>. We
-                could use your help by having
-                you
-                download the app for free and giving it a try. We appreciate any and all feedback to help us polish the
-                game
-                before it hits the app store.</p>
-            <a href="https://testflight.apple.com/join/5Vaqo19P">
-                <object data="{{ asset('pics/app-store-logo.svg') }}" width="200" class="app-store-icon p-3"
-                    style="pointer-events: none;"></object>
-                <p>Join the public beta today!</p>
-            </a>
-        </div>
-    </div>
-
-    <div class="container text-center">
-        <h2>Testimonials</h2>
-        <p>"Awesome two player mobile game! It was perfect to play while waiting in line at Disneyland! My 7 and 8 year
-            old boys love playing it as well!" - <b>Brady</b></p>
         <div class="row">
-            <div class="col-sm-4">
-                <img src="{{ asset('pics/disneyland-playing.jpg') }}" class="testimonial-image">
+            <div class="col-sm-6">
+                <h3>Features</h3>
+                <ul>
+                    <li>2 players on 1 device</li>
+                    <li>10 crazy colors to match</li>
+                    <li>12 unique powerups to choose from or have randomized</li>
+                    <li>90 second addictive gameplay</li>
+                    <li>3 challenging game modes (2 player clash with colors and powerups, 2 player clash with colors
+                        only,
+                        single player practice)</li>
+                    <li>Score tracking so you know how many taps you got right, wrong, and missed</li>
+                </ul>
+
+                <a href="https://apps.apple.com/us/app/crazy-color-clash/id1641436535?itsct=apps_box_badge&amp;itscg=30200"
+                    style="display: inline-block; overflow: hidden; border-radius: 13px; width: 250px; height: 83px;"><img
+                        src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1663718400&h=35a801062337f87a46a0f0c6b1f38ae1"
+                        alt="Download on the App Store" style="border-radius: 13px; width: 250px; height: 83px;"></a>
             </div>
-            <div class="col-sm-4">
-                <img src="{{ asset('pics/kids-playing-in-line.jpg') }}" class="testimonial-image">
-            </div>
-            <div class="col-sm-4">
-                <img src="{{ asset('pics/kids-playing.jpg') }}" class="testimonial-image">
+            <div class="col-sm-6 text-center">
+                <iframe width="476" height="847" src="https://www.youtube.com/embed/3rXwlH88iWU"
+                    title="Crazy Color Clash Teaser - iOS Game" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
             </div>
         </div>
     </div>
@@ -120,13 +119,13 @@
     <div class="bg-banner">
         <div class="container text-center">
             <h2>12 Exciting Powerups</h2>
-            <p>There are various powerups that are granted for correct taps. Use your powerups to confuse and disorient
+            <p>There are various powerups that are granted for correct taps. Use your powerups to confuse and
+                disorient
                 your
                 opponent!</p>
-            <p>During the public beta, all in-app purchases are <strong>free</strong>! Three powerups are included in
-                the
-                final release for
-                free while additional powerup packs can be purchased for $0.99.</p>
+            <p>Three powerups are included in the game for free (Surprise, Lockout, and Blackout) while additional
+                powerup packs can be purchased
+                for $0.99 or the entire set for $1.99.</p>
         </div>
     </div>
 
@@ -143,7 +142,8 @@
                 <div class="col">
                     <h4>Lockout</h4>
                     <img src="{{ asset('pics/lockout-min.png') }}" class="game-image img-fluid">
-                    <p>Locks your opponents screen. They must tap 5 times before they can match colors again. Powerups
+                    <p>Locks your opponents screen. They must tap 5 times before they can match colors again.
+                        Powerups
                         cannot be used during Lockout!</p>
                 </div>
                 <div class="col">
@@ -196,7 +196,8 @@
                 <div class="col">
                     <h4>Monochrome</h4>
                     <img src="{{ asset('pics/monochrome-min.png') }}" class="game-image img-fluid">
-                    <p>Changes your opponent's background to gray. They will need to guess if their colors match!</p>
+                    <p>Changes your opponent's background to gray. They will need to guess if their colors match!
+                    </p>
                 </div>
                 <div class="col">
                     <h4>Nuke</h4>
@@ -215,13 +216,15 @@
                 <div class="col">
                     <h4>Thief</h4>
                     <img src="{{ asset('pics/thief-min.png') }}" class="game-image img-fluid">
-                    <p>75% chance of stealing a point from your opponent. Be careful, the other 25% of the time you will
+                    <p>75% chance of stealing a point from your opponent. Be careful, the other 25% of the time you
+                        will
                         lose a point yourself!</p>
                 </div>
                 <div class="col">
                     <h4>Double Tap</h4>
                     <img src="{{ asset('pics/double-tap-min.png') }}" class="game-image img-fluid">
-                    <p>Requires your opponent to double tap their color on a match. Failing to double tap will lose a
+                    <p>Requires your opponent to double tap their color on a match. Failing to double tap will lose
+                        a
                         point!
                     </p>
                 </div>
@@ -234,14 +237,41 @@
         </div>
     </div>
 
+    <div class="bg-banner">
+        <div class="container text-center">
+            <h2>Testimonials</h2>
+            <p>"Awesome two player mobile game! It was perfect to play while waiting in line at Disneyland! My 7 and
+                8
+                year
+                old boys love playing it as well!" - <b>Brady</b></p>
+            <div class="row">
+                <div class="col">
+                    <img src="{{ asset('pics/disneyland-playing.jpg') }}" class="testimonial-image">
+                </div>
+                <div class="col">
+                    <img src="{{ asset('pics/kids-playing-in-line.jpg') }}" class="testimonial-image">
+                </div>
+                <div class="col">
+                    <img src="{{ asset('pics/kids-playing.jpg') }}" class="testimonial-image">
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="container text-center">
         <h2>Project Development</h2>
 
-        <p>The game started as a hobby to learn Swift and iOS development and quickly turned into a full-blown App Store
-            offering. Hundreds of hours and thousands of play tests later - Crazy Color Clash was brought to the public.
-            You can find devlog videos detailing the development of the project (early on).</p>
+        <p>The game started as a hobby to learn Swift and iOS development and quickly turned into a full-blown App
+            Store
+            offering. Hundreds of hours and thousands of play tests later - Crazy Color Clash was brought to the
+            public.
+            You can find devlog videos detailing the development of the project while it was in its infancy or read my
+            <a
+                href="https://blog.justinpaulhammond.com/justin-hammond/i-built-an-i-os-game-crazy-color-clash-heres-what-i-learned">blog
+                post</a> detailing the entire process.
+        </p>
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <div class="pa-video-container">
                     <iframe src="https://www.youtube.com/embed/DPlUEQtgA7U" title="YouTube video player"
                         frameborder="0"
@@ -250,10 +280,19 @@
                 </div>
             </div>
 
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <div class="pa-video-container">
                     <iframe src="https://www.youtube.com/embed/drPVDWmM3Sk" title="YouTube video player"
                         frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="pa-video-container">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/wB1RMOhW01U"
+                        title="YouTube video player" frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowfullscreen></iframe>
                 </div>
@@ -267,6 +306,7 @@
                 Policy</a>
             <a href="/terms-and-conditions" class="nav-link">Terms and
                 Conditions</a>
+            <a href="https://twitter.com/Justintime_50">Contact</a>
             <p>&#169; <a href="https://justinpaulhammond.com">Justin Hammond</a>, All rights reserved.</p>
         </div>
     </footer>

54dea74d6fee5c5c59f448f0d94355e4ac9c19b1
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-03-05 00:00:39 -0700

    fix: healthcheck

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # The following script will setup the project for the first time
 # To run the project in the future, simply run `docker compose up -d`
 
-main() {
+setup() {
     cd src || exit 1
 
     # Install dependencies
@@ -17,6 +17,19 @@ main() {
 
     # Spin up the project (assumes you already have Traefik setup)
     docker compose up -d --build --quiet-pull
+
+    # Run a healthcheck to ensure the container(s) are running
+    for ((ATTEMPT = 0; ATTEMPT <= 10; ATTEMPT += 1)); do
+        if healthcheck != 0; then
+            sleep 1
+        fi
+        exit 0
+    done
 }
 
-main
+healthcheck() {
+    docker ps | grep -q crazy-color-clash-site-crazy-color-clash-1 || exit 1
+    docker exec -t crazy-color-clash-site-crazy-color-clash-1 curl -f --silent --output /dev/null http://localhost || exit 1
+}
+
+setup
